6d19bcbfc20c006be7b69302010d2ded468dcb6d
Aidana Ualikhan <65579693+AbsoluteZero-cmd@users.noreply.github.com>
2023-04-08 13:41:53 +0600

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,10 +8,142 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  Widget _progressItem(IconData icon, String text, String points){
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.4,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.amber[200],
+                borderRadius: BorderRadius.all(Radius.circular(500))
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.all(Radius.circular(500))
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Icon(
+                      icon,
+                      color: Colors.amber[200],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('${text}:',),
+              Text(
+                points,
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 24
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: height * 0.4,
+          decoration: BoxDecoration(
+              color: Colors.amber,
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16))
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 42, horizontal: 16),
+            child: Column(
+              children: [
+                Align(
+                  alignment: AlignmentDirectional.topStart,
+                  child: Text(
+                    'Мой прогресс',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w800
+                    ),
+                  ),
+                ),
+                Align(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    width: width - 2 * 8.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Курс Алгебры: Пропорции',
+                          style: TextStyle(
+                              fontSize: 16
+                          ),
+                        ),
+                        Icon(
+                          Icons.calculate,
+                          color: Colors.amber,
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.amber[200],
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    margin: EdgeInsets.only(top: 30),
+                  ),
+                  alignment: AlignmentDirectional.topCenter,
+                ),
+                Spacer(),
+                Align(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _progressItem(Icons.check, 'Пройдено', '30'),
+                      _progressItem(Icons.timelapse, 'Потрачено', '42'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Популярные курсы:',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
