3be8dba6a1ebe6e5e93ca211fd22d3704531a485
Aidana Ualikhan <65579693+AbsoluteZero-cmd@users.noreply.github.com>
2023-04-08 12:17:22 +0600

    gjdfhkgjf

---
+++ lib/navigation.dart
@@ -0,0 +1,76 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_page.dart';
+import 'home_page.dart';
+import 'library_page.dart';
+import 'settings_page.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+
+  int _currentIndex = 0;
+  PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            Container(color: Colors.blueGrey,),
+            Container(color: Colors.red,),
+            Container(color: Colors.green,),
+            Container(color: Colors.blue,),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+              title: Text('Item One'),
+              icon: Icon(Icons.home)
+          ),
+          BottomNavyBarItem(
+              title: Text('Item Two'),
+              icon: Icon(Icons.apps)
+          ),
+          BottomNavyBarItem(
+              title: Text('Item Three'),
+              icon: Icon(Icons.chat_bubble)
+          ),
+          BottomNavyBarItem(
+              title: Text('Item Four'),
+              icon: Icon(Icons.settings)
+          ),
+        ],
+      ),
+    );
+  }
+}
